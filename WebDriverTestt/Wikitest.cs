d06042ea1af2cd7789e600286b14f1de4ffeb90d
Vili <Vili@DESKTOP-GN6P9GD>
2023-02-08 10:02:57 +0200

    Add project files.

---
+++ WebDriverTestt/Wikitest.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver=new ChromeDriver();
+driver.Url="https:/wikipedia.org";
+System.Console.WriteLine("Current Title: "+driver.Title);
+var searchFild = driver.FindElement(By.Id("searchInput"));
+searchFild.Click();
+searchFild.SendKeys("QA");
+searchFild.SendKeys(Keys.Enter);
+System.Console.WriteLine("Title: "+driver.Title);
+driver.Quit();
